0816b6a81d286239360c94439423da3daf13e6f9
aniketrick <aniket07chakraborty@gmail.com>
2023-06-12 20:43:06 +0530

    my first reactwebsite commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/Textform';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
   const[mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -38,18 +37,19 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
       <Navbar title="TextUtils" mode= {mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       <div className="container my-3">
-      <Routes>
+      {/* <Routes>
           <Route exact path="/about" element={<About />}>
-          </Route>
-          <Route exact path="/" element= {<TextForm showAlert ={showAlert} heading ="Enter the text to analyze" mode={mode} />}> 
-          </Route>
-        </Routes> 
+          </Route> */}
+          {/* <Route exact path="/" element= {<TextForm showAlert ={showAlert} heading ="Enter the text to analyze" mode={mode} />}>  */}
+          <TextForm showAlert ={showAlert} heading ="Enter the text to analyze" mode={mode} />
+          {/* </Route>
+        </Routes>  */}
       </div>
-        </Router>
+         {/* </Router> */}
     </>
   );
 }
